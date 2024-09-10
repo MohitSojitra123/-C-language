bcb9e7c35731e193b0b60d556a943ca5b786972e
MohitSojitra123 <mohitsojitra599@gmail.com>
2024-09-10 15:47:24 +0530

    program

---
+++ Day 2/patt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a;
+
+    printf("Enter Value..");
+    scanf("%d",&a);
+
+    printf("- - - - - - - - -\n");
+    printf("|\t \t|\n");
+    printf("|  5 x 5 = %d", a*a);
+    printf("\t|\n");
+    printf("|\t \t|\n");
+    printf("- - - - - - - - -\n");
+}
+
