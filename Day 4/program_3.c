dc8d745fa10cd1ffcdaa5eb6156fad18b0edbe71
MohitSojitra123 <mohitsojitra599@gmail.com>
2024-09-19 14:18:49 +0530

    program

---
+++ Day 4/program_3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int a,b,c;
+
+    printf("Enter Frist Angle....");
+    scanf("%d",&a);
+
+    printf("Enter Second Angle....");
+    scanf("%d",&b);
+    
+     int total=a+b;
+
+     printf("Thrid Angle Is...%d",(180-total));
+
+
+
+
+}
